ee723c61556db646455d5c14c82d047dd5974ce7
Luan Melo <luanmelolima@gmail.com>
2023-10-20 10:38:05 -0300

    correção de pastas

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,1 +1,22 @@
-export const App = () => 'Meu app'
+import * as React from 'react'
+import styled from 'styled-components'
+import { Theme } from './../components/Theme'
+
+const BaseButton = ({ className, children }) => (
+  <div className={className}>
+    MEU BOTÃO
+    {children}
+  </div>
+)
+const Button = styled(BaseButton)`
+  background: ${props => props.theme.colors[props.bg]};
+`
+
+export const App = () => (
+  <Theme>
+    <Button bg="blue">Meu App</Button>
+    <Button bg="green">Meu App</Button>
+    <Button bg="red">Meu App</Button>
+    <Button bg="yellow ">Meu App</Button>
+  </Theme>
+)
